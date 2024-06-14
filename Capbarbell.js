37f566ebd9e6d7c82ba5020e0bee84be0a58db68
mayy1980 <mayy1980@hotmail.com>
2024-06-14 02:57:37 -0500

    Update Capbarbell.js

--- Capbarbell.js
+++ Capbarbell.js
@@ -19,7 +19,7 @@
     $(function(){
         // 把百度的标题，直接改成暴力猴
         //$(document).attr("title","暴力猴");
-        //console.log($(location)[0].href);33
+        //console.log($(location)[0].href);44
         if ($(location)[0].href.includes("Module/AutoPO/AutoPoIII.aspx")){
             addGlobalStyle("#divGeneral{overflow-y: auto; }#TabContainer_tabGeneral_grdGeneral tr:first-of-type{position: sticky;top: 0;} td.locked{position: inherit;}");
             var tableCssTemplate="#divInbound4All{overflow-y: auto; }#TabContainer_tabInbound4All_grdInbound4All tr:nth-child(1){position: sticky;top: 0;} #TabContainer_tabInbound4All_grdInbound4All tr:nth-child(2){position: sticky;top: 55px;}";
